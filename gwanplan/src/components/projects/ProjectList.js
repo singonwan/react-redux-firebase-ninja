5e9e6020254c3c221ba54c270d54990a8ecc522b
George Wan <singonwan@gmail.com>
2019-08-20 11:57:46 -0400

    lesson 6 did project summary component

--- gwanplan/src/components/projects/ProjectList.js
+++ gwanplan/src/components/projects/ProjectList.js
@@ -1,32 +1,13 @@
 import React from 'react'
+import ProjectSummary from './ProjectSummary'
 
 const ProjectList = () => {
     return (
         <div className="project-list section">
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Gwan Ninja</p>
-                    <p className="grey-text">20th August, 11:30AM</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Gwan Ninja</p>
-                    <p className="grey-text">20th August, 11:30AM</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Gwan Ninja</p>
-                    <p className="grey-text">20th August, 11:30AM</p>
-                </div>
-            </div>
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
         </div>
     )
 }
