d322bfd93e80e8dacc529a16ef6a713867fffa30
George Wan <singonwan@gmail.com>
2019-08-20 11:44:13 -0400

    lesson 5, filled in dashboard components including projectlist

---
+++ gwanplan/src/components/projects/ProjectList.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const ProjectList = () => {
+    return (
+        <div className="project-list section">
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Gwan Ninja</p>
+                    <p className="grey-text">20th August, 11:30AM</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Gwan Ninja</p>
+                    <p className="grey-text">20th August, 11:30AM</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Gwan Ninja</p>
+                    <p className="grey-text">20th August, 11:30AM</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectList
