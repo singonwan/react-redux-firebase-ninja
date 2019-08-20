d1b4ab2b432adedc27543009c076b1b1968d221e
George Wan <singonwan@gmail.com>
2019-08-20 16:46:52 -0400

    adding dummy data to test grabbing information from redux store

--- gwanplan/src/components/dashboard/Dashboard.js
+++ gwanplan/src/components/dashboard/Dashboard.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 import Notifications from './Notifications'
 import ProjectList from '../projects/ProjectList'
+import { connect } from 'react-redux'
 
 class Dashboard extends Component {
     render(){
+        // console.log(this.props)
+        const { projects } = this.props;
+
         return(
             <div className="dashboard container">
                 <div className="row">
                     <div className="col s12 m6">
-                        <ProjectList />
+                        <ProjectList projects={projects} />
                     </div>
                     <div className="col s12 m5 offset-m1">
                         <Notifications />
@@ -18,5 +22,11 @@ class Dashboard extends Component {
         )
     }
 }
+//allows us to get data from store using props
+const mapStateToProps = (state) => {
+    return {
+        projects: state.project.projects
+    }
+}
 
-export default Dashboard
+export default connect(mapStateToProps)(Dashboard)
