a03d18f37beb7a9904bddea51644325cdcc46923
George Wan <singonwan@gmail.com>
2019-08-22 14:37:31 -0400

    built app put it in dist and deployed on firebase. DONE

---
+++ gwanplan/dist/precache-manifest.87fd0319323f3e2046dff3a1a6a420e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d685f594eb018acc2fb8dd763dee801b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d80b09cbda54fd39895",
+    "url": "/static/css/main.b856547e.chunk.css"
+  },
+  {
+    "revision": "9fbaa3e6180be39b12b5",
+    "url": "/static/js/2.1d1ab543.chunk.js"
+  },
+  {
+    "revision": "9d80b09cbda54fd39895",
+    "url": "/static/js/main.1d581fac.chunk.js"
+  },
+  {
+    "revision": "9884f517a8fef638e409",
+    "url": "/static/js/runtime~main.c125782f.js"
+  }
+]);
