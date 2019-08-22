fd4bbcd5a03898283617ae3ac071efc0641ec024
George Wan <singonwan@gmail.com>
2019-08-22 10:11:08 -0400

    adding projects with correct name now that there is profile

--- gwanplan/src/store/actions/projectActions.js
+++ gwanplan/src/store/actions/projectActions.js
@@ -2,11 +2,14 @@ export const createProject = (project) => {
     return(dispatch, getState, { getFirebase, getFirestore }) => {
         //make async call to db
         const firestore = getFirestore();
+        const profile = getState().firebase.profile;
+        const authorId = getState().firebase.auth.uid;
+
         firestore.collection('projects').add({
             ...project,
-            authorFirstName: 'gwen',
-            authorLastName: 'wen',
-            authorId: 12345,
+            authorFirstName: profile.firstName,
+            authorLastName: profile.lastName,
+            authorId: authorId,
             createdAt: new Date()
         }).then(() => {
             dispatch({ type: 'CREATE_PROJECT', project });
