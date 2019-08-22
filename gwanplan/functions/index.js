f9a7ca0ab1c17474737edc55a3d2a184d8fc85bd
George Wan <singonwan@gmail.com>
2019-08-22 14:06:50 -0400

    done cloud function auth trigger

--- gwanplan/functions/index.js
+++ gwanplan/functions/index.js
@@ -19,11 +19,26 @@ exports.projectCreated = functions.firestore
     .document('projects/{projectId}')
     .onCreate(doc =>{
         const project = doc.data();
-        const notifaction = {
+        const notification = {
             content: 'Added a new project',
             user: `${project.authorFirstName} ${project.authorLastName}`,
             time: admin.firestore.FieldValue.serverTimestamp()
         }
 
-        return createNotification(notifaction);
+        return createNotification(notification);
+    })
+
+exports.userJoined = functions.auth.user()
+    .onCreate(user => {
+        return admin.firestore().collection('users')
+            .doc(user.uid).get().then(doc => {
+                const newUser = doc.data();
+                const notification = {
+                    content: 'Joined the Party!',
+                    user: `${newUser.firstName} ${newUser.lastName}`,
+                    time: admin.firestore.FieldValue.serverTimestamp()
+                }
+
+                return createNotification(notification);
+            })
     })
