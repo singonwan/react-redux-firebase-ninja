9f51caae3b6cbdf935733e4c57f39a196a2ce4ae
George Wan <singonwan@gmail.com>
2019-08-22 10:18:20 -0400

    31 formatted date with moment

--- gwanplan/src/components/projects/ProjectDetails.js
+++ gwanplan/src/components/projects/ProjectDetails.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
 import { Redirect } from 'react-router-dom'
+import moment from 'moment'
 
 const ProjectDetails = (props) => {
     const { auth, project } = props;
@@ -18,7 +19,7 @@ const ProjectDetails = (props) => {
                     </div>
                     <div className="card-action grey lighten-4 grey-text">
                         <div>Posted by {project.authorFirstName} {project.authorLastName}</div>
-                        <div>Aug 20, 12pm</div>
+                        <div>{moment(project.createdAt.toDate()).calendar()}</div>
                     </div>
                 </div>
             </div>
